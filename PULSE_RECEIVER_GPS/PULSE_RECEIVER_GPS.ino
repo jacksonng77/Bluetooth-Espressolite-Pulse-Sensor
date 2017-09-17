05569497d79e17e4f71ad4add57f34ac169f6a13
Jackson Ng <jacksonng@outlook.com>
2017-09-17 15:50:17 +0800

    Update PULSE_RECEIVER_GPS.ino

--- PULSE_RECEIVER_GPS/PULSE_RECEIVER_GPS.ino
+++ PULSE_RECEIVER_GPS/PULSE_RECEIVER_GPS.ino
@@ -2,11 +2,6 @@
 #include <SoftwareSerial.h>
 #include <ESPert.h>
 
-/*
-   This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.
-   It requires the use of SoftwareSerial, and assumes that you have a
-   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
-*/
 static const int RXPin = 13, TXPin = 15; //rx 13 tx 15 rx
 static const uint32_t GPSBaud = 9600;
 
